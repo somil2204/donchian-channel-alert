ac1684fba65239a1c553a6c24c73537b18dc3367
somil2204 <2204.somil@gmail.com>
2021-10-07 22:54:10 +0530

    123

--- main.py
+++ main.py
@@ -1745,10 +1745,5 @@ for i in range(len(shares)):
     #writer.writerow(x)
     table.dataframe(df)
 
-towrite = io.BytesIO()
-downloaded_file = df.to_excel(towrite, encoding='utf-8', index=False, header=True)
-towrite.seek(0)  # reset pointer
-b64 = base64.b64encode(towrite.read()).decode()  # some strings
-linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="myfilename.xlsx">Download excel file</a>'
-st.markdown(linko, unsafe_allow_html=True)
+
 #a_file.close()    
